b764ea0b6751e86bb7e8a03c4ec7a0b4d2b264bc
Dijala Feludu <99227026+dfeludu4@users.noreply.github.com>
2022-08-05 18:00:08 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
  terraform {
    backend "s3" {
     bucket = "storing-our-tf-state"
-    key    = "state.tfstate"
+    key    = "dijolay/state.tfstate"
     region = "us-east-1"
     profile = "jespo"
     dynamodb_table = "terraform-state-lock"
